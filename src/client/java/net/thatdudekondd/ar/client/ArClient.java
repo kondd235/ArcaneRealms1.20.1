ce01b9873379310b2ffb5f0fd125253f19176431
kondd235 <69218038+kondd235@users.noreply.github.com>
2024-11-01 16:37:00 +0200

    Add files via upload

---
+++ src/client/java/net/thatdudekondd/ar/client/ArClient.java
@@ -0,0 +1,10 @@
+package net.thatdudekondd.ar.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/thatdudekondd/ar/Ar.java
@@ -0,0 +1,10 @@
+package net.thatdudekondd.ar;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ar implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
